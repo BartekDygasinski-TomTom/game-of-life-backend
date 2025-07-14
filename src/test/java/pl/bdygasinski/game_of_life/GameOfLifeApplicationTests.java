00ef87cda89b27bcfca7fdc8a6627e1de5affdf9
BartekDygasinski-TomTom <bartlomiej.dygasinski@tomtom.com>
2025-07-14 13:59:47 +0200

    Init

---
+++ src/test/java/pl/bdygasinski/game_of_life/GameOfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bdygasinski.game_of_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
